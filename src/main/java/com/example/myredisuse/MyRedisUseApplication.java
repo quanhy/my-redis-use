054d856ab67630592d7701a20194f15c0c5519ac
quanhy5@163.com <000888>
2020-01-14 14:34:38 +0800

    Initial commit

---
+++ src/main/java/com/example/myredisuse/MyRedisUseApplication.java
@@ -0,0 +1,13 @@
+package com.example.myredisuse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRedisUseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyRedisUseApplication.class, args);
+    }
+
+}
